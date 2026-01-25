df7de1e33fd90ecc2d23453c1c802598ba74ab5a
Cypher Playfair <70984583+Hendjf@users.noreply.github.com>
2026-01-25 17:19:18 -0500

    Recreate Movie table with structure and foreign key

--- scripts/delete_movie_id3.sql
+++ scripts/delete_movie_id3.sql
@@ -1,1 +1,13 @@
 -- delete_movie_id3.sql
+DROP TABLE IF EXISTS Movie;
+
+CREATE TABLE Movie (
+    Title VARCHAR(30),
+    RatingCode VARCHAR(5),
+    Genre VARCHAR(30),
+    Year INTEGER,
+    FOREIGN KEY (RatingCode) REFERENCES Rating(RatingCode)
+);
+
+SHOW COLUMNS FROM Movie;
+
