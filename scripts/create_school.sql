09a5d1aaed6c58c185fec9666485f5a0a0427384
Cypher Playfair <70984583+Hendjf@users.noreply.github.com>
2026-01-25 17:18:16 -0500

    Create School table with relevant columns

--- scripts/create_school.sql
+++ scripts/create_school.sql
@@ -1,1 +1,14 @@
 -- create_school.sql
+DROP TABLE IF EXISTS School;
+
+CREATE TABLE School (
+    SchoolID INTEGER,
+    SchoolName VARCHAR(50),
+    DateConstructed DATE,
+    OperatingBudget DECIMAL(3,1),
+    Classification CHAR(1),
+    President VARCHAR(20)
+);
+
+SHOW COLUMNS FROM School;
+
